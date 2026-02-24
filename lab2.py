4df89c84919446214affc5ffc1b999ad623cd825
cruz <cruzricardo23@gmail.com>
2026-02-24 01:45:34 +0000

    Added lab2 binary converter

---
+++ lab2.py
@@ -0,0 +1,14 @@
+#Ricardo Cruz
+#Lab2
+
+bit1 = int(input("Enter bit 1: "))  
+bit2 = int(input("Enter bit 2: "))  
+bit3 = int(input("Enter bit 3: "))  
+bit4 = int(input("Enter bit 4: "))
+bit5 = int(input("Enter bit 5: "))  
+bit6 = int(input("Enter bit 6: "))  
+
+decimal = (bit1 * 1) + (bit2 * 2) + (bit3 * 4) + (bit4 * 8) + (bit5 * 16) + (bit6 * 32)
+
+print("Your decimal number is", decimal)
+
