33d671cc530539be15259719148d58de8efebc07
cruz <cruzricardo23@gmail.com>
2026-03-17 16:40:34 +0000

    Add lab5revie

---
+++ lab5review.py
@@ -0,0 +1,17 @@
+#Ricardo Cruz
+#lab5 review
+
+name = input("Is this class hard?")
+print(" well you are either smart or you are either struggling. Select one or two")
+
+print("option 1")
+print("option 2")
+
+
+
+answer = int(input("select an option"))
+if (answer ==1):
+    print("Well I am struggling")
+
+elif (answer ==2):
+    print ("good luck on the midterm")
